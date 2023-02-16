1f007322385185942e5c34adfbb6d4fc5b08473e
Keerthi3275 <70685704+Keerthi3275@users.noreply.github.com>
2023-02-16 14:16:37 +0530

    Add files via upload

---
+++ movefiles.py
@@ -0,0 +1,30 @@
+#import OS & Shutil
+
+
+#Create two variables from_dir and to_dir to store source path and destination path, respectively
+
+
+#Create a variable, list_of_files , to store the names of all the files from the source path using os.listdir().
+#As given in specific task 4
+
+
+#Create a for-in loop to traverse through the list_of_files
+for file_name in list_of_files:
+
+    name, extension = os.path.splitext(file_name)
+    #Write an if condition to check if the extension is blank, if the condition is true then continue
+    if extension == '':
+        continue
+    #Create 3 variables for the name of the directory paths
+    if extension in ['.txt', '.pdf', '.doc', '.docx']:
+
+        path1 = from_dir + '/' + file_name                       # Example path1 : Downloads/ImageName1.jpg        
+        path2 = to_dir + '/' + "Document_Files"                     # Example path2 : D:/My Files/Image_Files      
+        path3 = to_dir + '/' + "Document_Files" + '/' + file_name   # Example path3 : D:/My Files/Image_Files/ImageName1.jpg
+
+#Check if the folder/directory path exists before moving using an if condition Else make a new folder/directory then move: (See Hint 1)
+
+
+
+
+
